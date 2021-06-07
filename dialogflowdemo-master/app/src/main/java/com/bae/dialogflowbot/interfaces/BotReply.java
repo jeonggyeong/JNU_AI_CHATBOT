21e8fb8adda6e413c73f66a2010ae03689511d52
suyeong heo <hshs208@naver.com>
2021-06-08 02:40:51 +0900

    0608

--- dialogflowdemo-master/app/src/main/java/com/bae/dialogflowbot/interfaces/BotReply.java
+++
@@ -1,9 +0,0 @@
-package com.bae.dialogflowbot.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-
-    void callback(DetectIntentResponse returnResponse);
-}
-
